4c1f15f6864206dcd2667da521b2da74ac54d35e
JustTrott <69697147+JustTrott@users.noreply.github.com>
2022-02-25 00:23:00 +0600

    upload

--- info-page.php
+++ info-page.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <?php
 $conn = mysqli_connect("localhost", "root", "", "shop");
+if(!isset($_GET['id'])){
+    header('Location: index.php');
+}
 ?>
 
 <html>
@@ -10,7 +13,7 @@ $conn = mysqli_connect("localhost", "root", "", "shop");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="info-page.css">
     <link rel="icon" href="favicon.ico">
 
 </head>
@@ -55,57 +58,21 @@ $conn = mysqli_connect("localhost", "root", "", "shop");
         <div class="show-login">Log in</div>
     </header>
     <section class="content">
-
-        <aside class="left-sidebar">
-            <div class="filter-title-box">
-                <h3 class="filter-title">Filter by: Genre</h3>
-            </div>
-            <ul class="genre-list">
-                <?php
-                $query = "SELECT genre_name FROM genres ORDER BY genre_name ASC";
-                $result = mysqli_query($conn, $query);
-                while($row = mysqli_fetch_array($result)) {
-                    echo "<li class='genre-item'><a href=\"index.php?genre={$row['genre_name']}\"";
-                    if(isset($_GET['genre']) && $_GET['genre'] == $row['genre_name']) {
-                        echo "style=\"font-weight: 1000;\"";
-                    }
-                    echo ">{$row['genre_name']}</a></li>";
-                }
-                ?>
-                <li class='genre-item clear-filters-button'>
-                    <a href='index.php'>Clear Filters</a>
-                </li>
-            </ul>
-        </aside>
-        <div class="cards">
-            <?php
-            if(isset($_GET['genre'])){
-                $query = "SELECT books.book_id, books.name, books.cover_path, books.price 
-                FROM books, genres, books_genres
-                WHERE genres.genre_name='{$_GET['genre']}' AND genres.genre_id = books_genres.genre_id AND books.book_id = books_genres.book_id";
-            }
-            else{
-                $query = "SELECT book_id, name, cover_path, price FROM books";
-            }
+            <?php 
+            $query = "SELECT description, name, price, cover_path FROM books WHERE book_id={$_GET['id']}";
             $result = mysqli_query($conn, $query);
-            while($row = mysqli_fetch_array($result)) {
+            $row = mysqli_fetch_array($result);
             ?>
-            <a class="card-container" href="info-page.php?id=<?php echo $row["book_id"]; ?>">
-                <div class="card">
-                    <div class="card-image"
-                        style="background-image: url('images/covers/<?php echo $row["cover_path"]; ?>.jpg');">
-                    </div>
-                    <div class="card-text-container">
-                        <p class="card-text"><?php echo $row["name"]; ?></p>
-                    </div>
-                    <div class="card-price-container">
-                        <p class="card-price">US $<?php echo $row["price"] ?></p>
-                    </div>
+        <div class="item-info">
+            <div class="main-text"><?php echo $row['name']; ?></div>
+            <div class="desc">
+                <div class="main-image">
+                    <img src="images/covers/<?php echo $row['cover_path']; ?>.jpg"
+                        class="desc-image" alt="<?php echo $row['name']; ?> COVER">
                 </div>
-            </a>
-            <?php
-            }
-            ?>
+                <div class="desc-text"><?php echo $row['description']; ?></p>
+                </div>
+            </div>
         </div>
     </section>
     <div class="footer">
